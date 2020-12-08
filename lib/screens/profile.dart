9e2ed363c6784dfc21ca20b8482adca13d3ec0e7
sjquant <seonujang92@gmail.com>
2020-12-09 01:03:59 +0900

    Add userbio and profileEditBtn

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -8,7 +8,19 @@ class ProfileScreen extends StatelessWidget {
         backgroundColor: Colors.grey[100],
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [_appbar(), _username()],
+          children: [
+            _appbar(),
+            Expanded(
+              child: CustomScrollView(slivers: [
+                SliverList(
+                    delegate: SliverChildListDelegate([
+                  _username(),
+                  _userbio(),
+                  _profileEditBtn(),
+                ]))
+              ]),
+            ),
+          ],
         ));
   }
 
@@ -35,6 +47,32 @@ class ProfileScreen extends StatelessWidget {
       child: Text(
         "Seonu Jang",
         style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget _userbio() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: COMMON_GAP),
+      child: Text(
+        "I earn what I believe!!!",
+        style: TextStyle(fontWeight: FontWeight.w400),
+      ),
+    );
+  }
+
+  Widget _profileEditBtn() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: COMMON_GAP),
+      child: SizedBox(
+        height: 24,
+        child: OutlineButton(
+            onPressed: null,
+            borderSide: BorderSide(color: Colors.black45),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+            child: Text("Edit Profile",
+                style: TextStyle(fontWeight: FontWeight.bold))),
       ),
     );
   }
