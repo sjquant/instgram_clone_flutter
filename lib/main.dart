6596a7856ef59e8887b63e690ccab6e3063c1bbd
sjquant <seonujang92@gmail.com>
2020-12-23 00:32:27 +0900

    Working on auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/constants/color.dart';
 import 'package:instagram_clone/homepage.dart';
+import 'package:instagram_clone/screens/auth.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: AuthScreen(),
+      // home: HomePage(),
       theme: ThemeData(primarySwatch: white),
     );
   }
--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/constants/duration.dart';
+import 'package:instagram_clone/widgets/signin_form.dart';
+import 'package:instagram_clone/widgets/signup_form.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -6,8 +9,39 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Widget _signupForm = SignupForm();
+  Widget _signinForm = SigninForm();
+
+  Widget _currentWidget;
+
+  @override
+  void initState() {
+    if (_currentWidget == null) _currentWidget = _signupForm;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SafeArea(
+      child: Stack(
+        children: [
+          AnimatedSwitcher(child: _currentWidget, duration: COMMON_DURATION),
+          Container(
+            child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    if (_currentWidget is SignupForm) {
+                      _currentWidget = _signinForm;
+                    } else {
+                      _currentWidget = _signupForm;
+                    }
+                  });
+                },
+                child: Text("Go to Sign up")),
+          ),
+        ],
+      ),
+    ));
   }
 }
