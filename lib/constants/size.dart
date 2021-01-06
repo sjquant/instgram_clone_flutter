b17f848d1923111d1355d1e2a38d69fb9b5c99ee
sjquant <seonujang92@gmail.com>
2021-01-07 01:18:26 +0900

    Create naive sign-up form layout

--- lib/constants/size.dart
+++ lib/constants/size.dart
@@ -1,3 +1,4 @@
 const double COMMON_GAP = 14.0;
+const COMMON_L_GAP = 16.0;
 const double COMMON_XXS_GAP = 8.0;
 const double AVATAR_SIZE = 30.0;
--- lib/widgets/signup_form.dart
+++ lib/widgets/signup_form.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/constants/size.dart';
 
-class SigninForm extends StatelessWidget {
+class SigninForm extends StatefulWidget {
+  @override
+  _SigninFormState createState() => _SigninFormState();
+}
+
+class _SigninFormState extends State<SigninForm> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepPurpleAccent,
-    );
+    return Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: COMMON_L_GAP,
+            ),
+            Image.asset("assets/images/insta_text_logo.png"),
+            TextField(),
+            TextField(),
+            TextField(),
+          ],
+        ));
   }
 }
