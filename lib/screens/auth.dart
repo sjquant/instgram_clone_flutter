e06d58b919a5e4fbe2dbe90eee91ba1d854cf2aa
sjquant <seonujang92@gmail.com>
2021-01-20 01:04:33 +0900

    Make form responsive to keyboard up

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -12,46 +12,48 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         body: SafeArea(
-      child: Stack(
-        children: [
-          FadeStack(
-            currentStackIndex: currentStackIndex,
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            height: 40,
-            child: Container(
-              color: Colors.white,
-              child: FlatButton(
-                shape: Border(top: BorderSide(color: Colors.grey[300])),
-                onPressed: () {
-                  setState(() {
-                    if (currentStackIndex == 0)
-                      currentStackIndex = 1;
-                    else
-                      currentStackIndex = 0;
-                  });
-                },
-                child: RichText(
-                    text: TextSpan(
-                        text: (currentStackIndex == 1)
-                            ? "Already have an account?  "
-                            : "Don't you have an account?  ",
-                        style: TextStyle(color: Colors.grey),
-                        children: [
-                      TextSpan(
-                          text:
-                              (currentStackIndex == 1) ? "Sign In" : "Sgin up",
-                          style: TextStyle(color: Colors.blue))
-                    ])),
+          child: Stack(
+            children: [
+              FadeStack(
+                currentStackIndex: currentStackIndex,
               ),
-            ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                height: 40,
+                child: Container(
+                  color: Colors.white,
+                  child: FlatButton(
+                    shape: Border(top: BorderSide(color: Colors.grey[300])),
+                    onPressed: () {
+                      setState(() {
+                        if (currentStackIndex == 0)
+                          currentStackIndex = 1;
+                        else
+                          currentStackIndex = 0;
+                      });
+                    },
+                    child: RichText(
+                        text: TextSpan(
+                            text: (currentStackIndex == 1)
+                                ? "Already have an account?  "
+                                : "Don't you have an account?  ",
+                            style: TextStyle(color: Colors.grey),
+                            children: [
+                          TextSpan(
+                              text: (currentStackIndex == 1)
+                                  ? "Sign In"
+                                  : "Sgin up",
+                              style: TextStyle(color: Colors.blue))
+                        ])),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 }
--- lib/widgets/signup_form.dart
+++ lib/widgets/signup_form.dart
@@ -22,86 +22,87 @@ class _SigninFormState extends State<SigninForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(COMMON_GAP),
-      child: Form(
-          key: _formKey,
-          child: ListView(
-            children: <Widget>[
-              SizedBox(
-                height: COMMON_L_GAP,
-              ),
-              Image.asset("assets/images/insta_text_logo.png"),
-              TextFormField(
-                  controller: _emailController,
-                  decoration: _decorateInput("Email"),
-                  validator: (text) {
-                    if (text.isNotEmpty && text.contains("@")) {
-                      return null;
-                    } else {
-                      return "정확한 이메일 주소를 입력해주세요.";
-                    }
-                  }),
-              SizedBox(height: COMMON_GAP),
-              TextFormField(
-                  controller: _pwController,
-                  obscureText: true,
-                  decoration: _decorateInput("Password"),
-                  validator: (text) {
-                    if (text.isNotEmpty && text.length > 7) {
-                      return null;
-                    } else {
-                      return "정확한 비밀번호를 입력해주세요.";
-                    }
-                  }),
-              SizedBox(height: COMMON_GAP),
-              TextFormField(
-                  controller: _cpwController,
-                  obscureText: true,
-                  decoration: _decorateInput("Confirm Password"),
-                  validator: (text) {
-                    if (text.isNotEmpty && text == _pwController.text) {
-                      return null;
-                    } else {
-                      return "비밀번호가 일치하지 않습니다.";
-                    }
-                  }),
-              SizedBox(height: COMMON_GAP),
-              FlatButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    print("Validation Success!!");
-                  }
-                },
-                child: Text(
-                  "Join",
-                  style: TextStyle(color: Colors.white),
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Padding(
+        padding: const EdgeInsets.all(COMMON_GAP),
+        child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                SizedBox(
+                  height: COMMON_L_GAP,
                 ),
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6)),
-              )
-            ],
-          )),
+                Image.asset("assets/images/insta_text_logo.png"),
+                TextFormField(
+                    controller: _emailController,
+                    decoration: _decorateInput("Email"),
+                    validator: (text) {
+                      if (text.isNotEmpty && text.contains("@")) {
+                        return null;
+                      } else {
+                        return "정확한 이메일 주소를 입력해주세요.";
+                      }
+                    }),
+                SizedBox(height: COMMON_GAP),
+                TextFormField(
+                    controller: _pwController,
+                    obscureText: true,
+                    decoration: _decorateInput("Password"),
+                    validator: (text) {
+                      if (text.isNotEmpty && text.length > 7) {
+                        return null;
+                      } else {
+                        return "정확한 비밀번호를 입력해주세요.";
+                      }
+                    }),
+                SizedBox(height: COMMON_GAP),
+                TextFormField(
+                    controller: _cpwController,
+                    obscureText: true,
+                    decoration: _decorateInput("Confirm Password"),
+                    validator: (text) {
+                      if (text.isNotEmpty && text == _pwController.text) {
+                        return null;
+                      } else {
+                        return "비밀번호가 일치하지 않습니다.";
+                      }
+                    }),
+                SizedBox(height: COMMON_GAP),
+                FlatButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      print("Validation Success!!");
+                    }
+                  },
+                  child: Text(
+                    "Join",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6)),
+                )
+              ],
+            )),
+      ),
     );
   }
 
   InputDecoration _decorateInput(String hint) {
     return InputDecoration(
         hintText: hint,
-        focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey[200]),
-            borderRadius: BorderRadius.circular(8)),
-        errorBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.red[200]),
-            borderRadius: BorderRadius.circular(8)),
-        focusedErrorBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.red[200]),
-            borderRadius: BorderRadius.circular(8)),
-        enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey[200]),
-            borderRadius: BorderRadius.circular(8)),
+        focusedBorder: _inputBorder(Colors.grey[200]),
+        enabledBorder: _inputBorder(Colors.grey[200]),
+        errorBorder: _inputBorder(Colors.red[200]),
+        focusedErrorBorder: _inputBorder(Colors.red[200]),
         filled: true,
         fillColor: Colors.grey[100]);
+  }
+
+  OutlineInputBorder _inputBorder(Color color) {
+    return OutlineInputBorder(
+        borderSide: BorderSide(color: color),
+        borderRadius: BorderRadius.circular(8));
   }
 }
